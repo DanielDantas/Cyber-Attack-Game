8c0e5ef099e846e26fe395a763bd153ce94ead14
Daniel Dantas <danieldnds@gmail.com>
2019-07-23 10:44:01 -0500

    zero/one bullets

--- Cyber-Attack/Assets/Scripts/BulletController.cs
+++ Cyber-Attack/Assets/Scripts/BulletController.cs
@@ -6,17 +6,42 @@ public class BulletController : MonoBehaviour
 {
     public float Speed = 20f;
     public int Damage = 1;
+    public Sprite Zero;
+    public Sprite One;
+    public GameObject ImpactEffect;
     public Rigidbody2D BulletRB;
 
+    private SpriteRenderer spriteRenderer;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        BulletRB.velocity = transform.right * Speed;    
+    void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer.sprite == null) {
+            spriteRenderer.sprite = Zero;
+        }
+        ChangeSprite();
+
+        BulletRB.velocity = transform.right * Speed;
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
         var enemy = collision.GetComponent<EnemyController>();
-        enemy?.TakeDamage(Damage);
+        if (enemy != null) {
+            enemy.TakeDamage(Damage);
+        }
+
+        Instantiate(ImpactEffect, transform.position, transform.rotation);
+
         Destroy(gameObject);
+    }
+
+    private void ChangeSprite() {
+        System.Random rand = new System.Random();
+
+        if (rand.Next(0, 2) == 0) {
+            spriteRenderer.sprite = Zero;
+        } else {
+            spriteRenderer.sprite = One;
+        }
     }
 }
