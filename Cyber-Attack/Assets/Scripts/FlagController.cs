19ba60ddeb57e64dffc5ea851dedcf5d1ce2a76d
Matthew Zent <mazent@microsoft.com>
2019-07-24 11:22:40 -0500

    flag

---
+++ Cyber-Attack/Assets/Scripts/FlagController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlagController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Sprite[] flags;
+    private int i = 0;
+    private bool offsetForward;
+    public bool on = false;
+    void Start()
+    {
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (on)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = flags[i / 5];
+            i++;
+            if (offsetForward)
+            {
+                i++;
+                if (i >= flags.Length * 5)
+                {
+                    i--;
+                    offsetForward = false;
+                }
+            }
+            else
+            {
+                i--;
+                if (i < 0)
+                {
+                    i++;
+                    offsetForward = true;
+                }
+            }
+        }
+    }
+
+    public void show(Vector3 catPos)
+    {
+        gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        transform.position = catPos;
+        on = true;
+    }
+}
