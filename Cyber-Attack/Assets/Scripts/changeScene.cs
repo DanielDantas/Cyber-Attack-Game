c9773e76ddf206ddbd15cdb71ae254bf0d5e8c96
Matthew Zent <mazent@microsoft.com>
2019-07-23 21:50:37 -0500

    moving start button

--- Cyber-Attack/Assets/Scripts/changeScene.cs
+++ Cyber-Attack/Assets/Scripts/changeScene.cs
@@ -6,12 +6,37 @@ using UnityEngine.SceneManagement;
 
 public class changeScene : MonoBehaviour
 {
+    private float[] offset = { 0, .05f, .1f, .2f, .25f, .3f };
+    private int i = 0;
+    private bool offsetForward = true;
+
+
     public void Update()
     {
         if (Input.GetButtonDown("Submit"))
         {
             ChangeScene("Main");
         }
+        transform.position = new Vector3(0, -3 + offset[i/5], 0);
+        if (offsetForward)
+        {
+            i++;
+            if (i >= offset.Length * 5)
+            {
+                i--;
+                offsetForward = false;
+            }
+        }
+        else
+        {
+            i--;
+            if (i < 0)
+            {
+                i++;
+                offsetForward = true;
+            }
+        }
+
     }
 
 
